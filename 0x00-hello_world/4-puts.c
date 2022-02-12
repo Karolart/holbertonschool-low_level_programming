84690a12c6eb274bd11b8fc32e1cc673dbe86c26
karolart <karoljuzz1610@gmail.com>
2022-02-11 20:16:05 -0800

    -CProgramPrints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Description: main - show in the screen a sentence , printi a string out.
+ * Return: 0 (always if Sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
