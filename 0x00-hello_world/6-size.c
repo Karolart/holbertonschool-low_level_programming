e4e54ef49fc6d4a18bf2b3d3a04a29afe4c4f4a1
karolart <karoljuzz1610@gmail.com>
2022-02-11 20:33:39 -0800

    prints the size of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - show in the screen the value of the diferent kind of variables.
+  *
+  * Return: 0(always if is sucess)
+  */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+	return (0);
+}
